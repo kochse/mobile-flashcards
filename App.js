b032a81005910bd96f70a1051de09c4ab54e96cc
Sebastian Koch <kochsebastian@gmail.com>
2017-12-24 01:25:37 +0100

    feat: Navigator

--- App.js
+++ App.js
@@ -1,14 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Constants } from 'expo';
+import reducer from './reducers';
 
-export default class App extends React.Component {
+class ListScreen extends React.Component {
+  state = {
+    decks: [
+      { id: 0, title: "Deck 1", cards: 0 },
+      { id: 1, title: "Deck 2", cards: 0 },
+      { id: 2, title: "Deck 3", cards: 0 },
+    ]
+  }
+
+  handleDeckSelection = () => {
+    this.props.navigation.navigate("Deck");
+  }
+
+  renderDeck = ({item}) => {
+    return (
+      <TouchableOpacity style={styles.deck} onPress={this.handleDeckSelection}>
+        <Text>{item.title}</Text>
+        <Text>{item.cards} cards</Text>
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList
+          data={this.state.decks}
+          renderItem={this.renderDeck}
+          keyExtractor={(item) => item.id }
+        />
       </View>
+    );
+  }
+}
+
+class SingleDeckScreen extends React.Component {
+  render() {
+    return (
+      <Text>Single Screen</Text>
+    );
+  }
+}
+
+
+const MainNavigator = StackNavigator({
+  List: {
+    screen: ListScreen,
+  },
+  Deck: {
+    screen: SingleDeckScreen,
+  },
+});
+
+export default class App extends React.Component {
+
+
+  render() {
+
+    return (
+      <Provider store={createStore(reducer)}>
+        <MainNavigator/>
+      </Provider>
     );
   }
 }
@@ -17,7 +76,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+
+  },
+  statusBar: {
+    backgroundColor: "#C2185B",
+    height: Constants.statusBarHeight,
+  },
+  deck: {
+    height: 120,
+    borderBottomWidth: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
