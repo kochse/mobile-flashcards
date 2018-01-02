9e34a9ae701b423ea909b5173f3cf68657e6e931
Sebastian Koch <kochsebastian@gmail.com>
2018-01-02 01:08:56 +0100

    fix: navigation

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { connect } from 'react-redux';
+import { NavigationActions } from 'react-navigation';
 import { addDeck } from '../actions';
 import { saveDeck } from '../utils/api';
 import { uuidv4 } from '../utils/helpers';
@@ -18,11 +19,19 @@ class AddDeck extends React.Component {
 
   handleSubmit = () => {
     const { title } = this.state;
-    const { addDeck, goBack } = this.props;
-    const deck = { [uuidv4()]: { title, questions: [] } };
+    const { addDeck, navigation } = this.props;
+    const key = uuidv4();
+    const deck = { [key]: { title, questions: [] } };
     addDeck(deck);
     saveDeck(deck);
-    goBack();
+    const resetAction = NavigationActions.reset({
+      index: 1,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Decks'}),
+        NavigationActions.navigate({ routeName: 'DeckDetail', params: { key, title } }),
+      ],
+    });
+    navigation.dispatch(resetAction);
     this.setState({ ...this.state, title: '' });
   };
 
@@ -42,11 +51,4 @@ const styles = StyleSheet.create({
   },
 });
 
-const mapDispatchToProps = (dispatch, { navigation }) => ({
-  addDeck: deck => dispatch(addDeck(deck)),
-  goBack: () => navigation.goBack(),
-});
-
-const mapStateToProps = () => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(AddDeck);
+export default connect(undefined, { addDeck })(AddDeck);
